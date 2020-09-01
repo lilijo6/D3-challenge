cb2ec894b1e0f5d792e4d02a721103969ff52cab
lilijo6 <64238731+lilijo6@users.noreply.github.com>
2020-09-01 14:31:12 -0400

    adding axis and trying to add text to the bubbles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -44,7 +44,7 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     // create scales
     var xScale = d3.scaleLinear()
         // using extent to grab the range of the censusData
-        .domain([d3.min(censusData, d => d.poverty) - 3, d3.max(censusData, d => d.poverty)])
+        .domain([d3.min(censusData, d => d.poverty) * 0.8, d3.max(censusData, d => d.poverty)])
         .range([0, chartWidth]);
 
     var yLinearScale = d3.scaleLinear()
@@ -75,35 +75,79 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
         .attr("fill", "blue")
         .attr("opacity", ".5");
 
-    //Add the text for the State in the middle of the circle
-    var texts = chartGroup.append('text')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline', 'middle')
-        .style('.stateCircle', d => d.radius * 0.4 + 'px')
-        .attr('fill-opacity', 0)
-        .attr('fill', 'white')
-        .text(d => d.abbr)
-        // // Transitions copied from https://bl.ocks.org/d3noob/899a0b2490318a96f9ebd40a5a84e4a7
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.state}<br>Poverty: ${d.poverty}% <br>Healthcare: ${d.healthcare}%`);
+        });
 
-    // var svg = d3.select("body") // Select the body element
-    //     .append("svg") // Append an SVG element to the body
-    //     .attr("width", 960) // make it 960 pixels wide
-    //     .attr("height", 500) // make it 500 pixels high
-    //     .append("circle") // append a circle to the svg
-    //     .style("fill", "blue") // fill the circle with 'blue'
-    //     .attr("r", 20) // set the radius to 10 pixels
-    //     .attr('cx', 40) // position the circle at 40 on the x axis
-    //     .attr('cy', 250) // position the circle at 250 on the y axis
-    //     // 1st transition  
-    //     .transition() // apply a transition
-    //     .duration(4000) // apply it over 4000 milliseconds
-    //     .attr('cx', 920) // new horizontal position at 920 on x axis
-    //     // 2nd transition
-    //     .transition() // apply a transition
-    //     .duration(4000) // apply it over 4000 milliseconds
-    //     .attr('r', 40) // new radius of 40 pixels
-    //     // 3rd transition
-    //     .transition() // apply a transition
-    //     .duration(4000) // apply it over 4000 milliseconds
-    //     .style('fill', "red");
-})
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+            toolTip.show(data, this);
+        })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks in Healthcare (%)");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
+
+}).catch(function(error) {
+    console.log(error);
+});
+
+
+// //Add the text for the State in the middle of the circle
+// var texts = chartGroup.selectAll(".text")
+//     .enter()
+//     .append('text')
+//     .attr('text-anchor', 'middle')
+//     .attr('alignment-baseline', 'middle')
+//     .style('.stateCircle', d => d.radius * 0.4 + 'px')
+//     .attr('fill-opacity', 0)
+//     .attr('fill', 'white')
+//     .text(d => d.abbr)
+//     // // Transitions copied from https://bl.ocks.org/d3noob/899a0b2490318a96f9ebd40a5a84e4a7
+
+// // var svg = d3.select("body") // Select the body element
+// //     .append("svg") // Append an SVG element to the body
+// //     .attr("width", 960) // make it 960 pixels wide
+// //     .attr("height", 500) // make it 500 pixels high
+// //     .append("circle") // append a circle to the svg
+// //     .style("fill", "blue") // fill the circle with 'blue'
+// //     .attr("r", 20) // set the radius to 10 pixels
+// //     .attr('cx', 40) // position the circle at 40 on the x axis
+// //     .attr('cy', 250) // position the circle at 250 on the y axis
+// //     // 1st transition  
+// //     .transition() // apply a transition
+// //     .duration(4000) // apply it over 4000 milliseconds
+// //     .attr('cx', 920) // new horizontal position at 920 on x axis
+// //     // 2nd transition
+// //     .transition() // apply a transition
+// //     .duration(4000) // apply it over 4000 milliseconds
+// //     .attr('r', 40) // new radius of 40 pixels
+// //     // 3rd transition
+// //     .transition() // apply a transition
+// //     .duration(4000) // apply it over 4000 milliseconds
+// //     .style('fill', "red");
+// })
